4b29f52b70ffdf6bd3181dfe3e441bb9c4c2f1e1
Pierre-Chesne <pierrc@hotmail.com>
2020-03-31 17:13:58 +0200

    v0.1

---
+++ Prerequis/DSC/InitDisk.ps1
@@ -0,0 +1,26 @@
+﻿configuration InitDisk
+{ 
+    param 
+    () 
+    
+    Import-DscResource -ModuleName xStorage
+
+    Node localhost
+    {
+        LocalConfigurationManager {
+            RebootNodeIfNeeded = $true
+        }
+        xWaitforDisk Disk2
+        {
+            DiskNumber = 2
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+        }
+
+        xDisk ADDataDisk {
+            DiskNumber  = 2
+            DriveLetter = "F"
+            DependsOn   = "[xWaitForDisk]Disk2"
+        }
+    }
+} 
